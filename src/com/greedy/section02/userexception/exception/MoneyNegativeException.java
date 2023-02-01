226cb7f3a81c05a631909f94ecac60900b826bd4
ljsung <ljsung6@naver.com>
2023-02-01 18:43:54 +0900

    챕터 13

---
+++ src/com/greedy/section02/userexception/exception/MoneyNegativeException.java
@@ -0,0 +1,10 @@
+package com.greedy.section02.userexception.exception;
+
+public class MoneyNegativeException extends NegativeException{
+
+	public MoneyNegativeException() {}
+
+	public MoneyNegativeException(String message) {
+		super(message);
+	}
+}
